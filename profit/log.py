e8acfa5f0a1df4fc069e5046ce8154371eba97a7
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2020-12-15 17:00:19 +0300

    Add XES reader in Log

--- profit/log.py
+++ profit/log.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pm4py
 
 class Log(object):
     """Perform event log object from a log-file.
@@ -23,21 +24,41 @@ class Log(object):
         self.cases = set()
         self.activities = set()
 
+    def read_xes(self, FILE_PATH):
+        """Read XES file into DataFrame."""
+        log = pm4py.read_xes(FILE_PATH)
+        df = pd.DataFrame([], columns=['ID', 'Activity', 'TimeStamp'])
+        trace_id, activity, timestamp  = [], [], []
+        for i,events in enumerate(log):
+            for e in events:
+                trace_id.append(i)
+                activity.append(e['concept:name'])
+                timestamp.append(e['time:timestamp'])
+        df['ID'] = trace_id
+        df['Activity'] = activity
+        df['TimeStamp'] = timestamp
+        df = df.sort_values('TimeStamp').drop('TimeStamp', axis=1)
+
+        return df
+
     def update(self, data=None, FILE_PATH='', cols=(0,1), *args, **kwargs):
         """Update attributes via file reading.
         
         Parameters
         ----------
         data: DataFrame
-        	Log-file as DataFrame
+            Log-file as DataFrame
         FILE_PATH: str
-            Path to the CSV log-file (alternative to data)
+            Path to the CSV/TXT/XES log-file (alternative to data)
         cols: tuple
             Columns in the log-file to use as case id and activity
             attributes, respectively (default (0,1))
         """
         if FILE_PATH:
-        	log = pd.read_csv(FILE_PATH, usecols=cols, *args, **kwargs)
+            if FILE_PATH[-4:] == ".xes":
+                log = self.read_xes(FILE_PATH)
+            else:
+                log = pd.read_csv(FILE_PATH, usecols=cols, *args, **kwargs)
         else:
             log = data.iloc[:, list(cols)]
         log.columns = ['case_id', 'activity']
